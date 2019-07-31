4a20678305cb0a591f1805c998dd3d8a8667110e
Mr. Fluffy <git@sixearsm>
2019-07-31 12:42:41 +0000

    factor out FPath.T.FPath.PathComponent

--- src/FPath/T/FPath.hs
+++ src/FPath/T/FPath.hs
@@ -10,49 +10,13 @@ where
 
 -- base --------------------------------
 
-import Data.Function    ( ($) )
-import Data.Maybe       ( Maybe( Nothing ) )
 import Data.String      ( String )
 import Numeric.Natural  ( Natural )
 import System.IO        ( IO )
 
--- base-unicode-symbols ----------------
-
-import Data.Eq.Unicode  ( (≢) )
-
--- data-textual ------------------------
-
-import Data.Textual  ( Parsed( Parsed ), fromString, parseString, toText )
-
--- genvalidity -------------------------
-
-import Data.GenValidity  ( genValid )
-
--- genvalidity-property ----------------
-
-import Test.Validity.GenValidity.Property  ( genGeneratesValid )
-
--- more-unicode ------------------------
-
-import Data.MoreUnicode.Tasty  ( (≟) )
-
--- QuickCheck --------------------------
-
-import Test.QuickCheck.Property  ( property )
-
 -- tasty -------------------------------
 
 import Test.Tasty  ( TestTree, testGroup )
-
--- tasty-hunit -------------------------
-
-import Test.Tasty.HUnit  ( testCase )
-
--- tasty-quickcheck --------------------
-
-import Test.Tasty.QuickCheck  ( Arbitrary( arbitrary ), Gen, Property
-                              , shrink, testProperty
-                              )
 
 ------------------------------------------------------------
 --                     local imports                      --
@@ -60,69 +24,15 @@ import Test.Tasty.QuickCheck  ( Arbitrary( arbitrary ), Gen, Property
 
 import qualified  FPath.T.FPath.AbsDir
 import qualified  FPath.T.FPath.NonRootAbsDir
+import qualified  FPath.T.FPath.PathComponent
 import qualified  FPath.T.FPath.RelDir
-
-import FPath.PathComponent  ( PathComponent, pc )
 
 import FPath.T.Common       ( doTest, doTestR, doTestS )
 
 --------------------------------------------------------------------------------
 
-pathCArbitraryTests ∷ TestTree
-pathCArbitraryTests =
-  let propNonEmpty ∷ PathComponent → Property
-      propNonEmpty p = property $ toText p ≢ ""
-   in testProperty "non-empty" propNonEmpty
-
-pathCTextualTests ∷ TestTree
-pathCTextualTests =
-  let nothin'     ∷ Maybe PathComponent
-      nothin'     = Nothing
-      success e s = testCase s $ Parsed e  ≟ parseString s
-      fail s      = testCase s $ nothin'   ≟ fromString s
-   in testGroup "Textual" [ success [pc|etc|]   "etc"
-                          , success [pc|pam.d|] "pam.d"
-                          , success [pc|.d|] ".d"
-                          , success [pc|..d|] "..d"
-                          , success [pc|d..|] "d.."
-                          , success [pc|d.|] "d."
-                          , fail "."
-                          , fail "/."
-                          , fail "./"
-                          , fail "/./"
-                          , fail ".."
-                          , fail "/.."
-                          , fail "../"
-                          , fail "/../"
-                          , fail "/etc"
-                          , fail "etc/"
-                          , fail "e/c"
-                          , fail "\0etc"
-                          , fail "etc\0"
-                          , fail "e\0c"
-                          ]
-
-pathCValidityTests ∷ TestTree
-pathCValidityTests =
-  let genValidPC ∷ Gen PathComponent
-      genValidPC = genValid
-      arbPC ∷ Gen PathComponent
-      arbPC = arbitrary
-   in testGroup "Validity"
-                [
-                  testProperty "genValid"  $ genGeneratesValid genValidPC shrink
-                , testProperty "arbitrary" $ genGeneratesValid arbPC      shrink
-                ]
-
-pathComponentTests ∷ TestTree
-pathComponentTests =
-  testGroup "PathComponent" [ pathCArbitraryTests
-                            , pathCTextualTests, pathCValidityTests ]
-
-----------------------------------------
-
 tests ∷ TestTree
-tests = testGroup "FPath" [ pathComponentTests
+tests = testGroup "FPath" [ FPath.T.FPath.PathComponent.tests
                           , FPath.T.FPath.AbsDir.tests
                           , FPath.T.FPath.NonRootAbsDir.tests
                           , FPath.T.FPath.RelDir.tests
---
+++ src/FPath/T/FPath/PathComponent.hs
@@ -0,0 +1,133 @@
+{-# LANGUAGE FlexibleContexts  #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
+{-# LANGUAGE TypeApplications  #-}
+{-# LANGUAGE UnicodeSyntax     #-}
+
+module FPath.T.FPath.PathComponent
+  ( tests )
+where
+
+-- base --------------------------------
+
+import Data.Function    ( ($) )
+import Data.Maybe       ( Maybe( Nothing ) )
+import Data.String      ( String )
+import Numeric.Natural  ( Natural )
+import System.IO        ( IO )
+
+-- base-unicode-symbols ----------------
+
+import Data.Eq.Unicode  ( (≢) )
+
+-- data-textual ------------------------
+
+import Data.Textual  ( Parsed( Parsed ), fromString, parseString, toText )
+
+-- genvalidity -------------------------
+
+import Data.GenValidity  ( genValid )
+
+-- genvalidity-property ----------------
+
+import Test.Validity.GenValidity.Property  ( genGeneratesValid )
+
+-- more-unicode ------------------------
+
+import Data.MoreUnicode.Tasty  ( (≟) )
+
+-- QuickCheck --------------------------
+
+import Test.QuickCheck.Property  ( property )
+
+-- tasty -------------------------------
+
+import Test.Tasty  ( TestTree, testGroup )
+
+-- tasty-hunit -------------------------
+
+import Test.Tasty.HUnit  ( testCase )
+
+-- tasty-quickcheck --------------------
+
+import Test.Tasty.QuickCheck  ( Arbitrary( arbitrary ), Gen, Property
+                              , shrink, testProperty
+                              )
+
+------------------------------------------------------------
+--                     local imports                      --
+------------------------------------------------------------
+
+import FPath.PathComponent  ( PathComponent, pc )
+
+import FPath.T.Common       ( doTest, doTestR, doTestS )
+
+--------------------------------------------------------------------------------
+
+pathCArbitraryTests ∷ TestTree
+pathCArbitraryTests =
+  let propNonEmpty ∷ PathComponent → Property
+      propNonEmpty p = property $ toText p ≢ ""
+   in testProperty "non-empty" propNonEmpty
+
+pathCTextualTests ∷ TestTree
+pathCTextualTests =
+  let nothin'     ∷ Maybe PathComponent
+      nothin'     = Nothing
+      success e s = testCase s $ Parsed e  ≟ parseString s
+      fail s      = testCase s $ nothin'   ≟ fromString s
+   in testGroup "Textual" [ success [pc|etc|]   "etc"
+                          , success [pc|pam.d|] "pam.d"
+                          , success [pc|.d|] ".d"
+                          , success [pc|..d|] "..d"
+                          , success [pc|d..|] "d.."
+                          , success [pc|d.|] "d."
+                          , fail "."
+                          , fail "/."
+                          , fail "./"
+                          , fail "/./"
+                          , fail ".."
+                          , fail "/.."
+                          , fail "../"
+                          , fail "/../"
+                          , fail "/etc"
+                          , fail "etc/"
+                          , fail "e/c"
+                          , fail "\0etc"
+                          , fail "etc\0"
+                          , fail "e\0c"
+                          ]
+
+pathCValidityTests ∷ TestTree
+pathCValidityTests =
+  let genValidPC ∷ Gen PathComponent
+      genValidPC = genValid
+      arbPC ∷ Gen PathComponent
+      arbPC = arbitrary
+   in testGroup "Validity"
+                [
+                  testProperty "genValid"  $ genGeneratesValid genValidPC shrink
+                , testProperty "arbitrary" $ genGeneratesValid arbPC      shrink
+                ]
+
+tests ∷ TestTree
+tests =
+  testGroup "PathComponent" [ pathCArbitraryTests
+                            , pathCTextualTests, pathCValidityTests ]
+
+----------------------------------------
+
+-- Cannot use Fluffy.Tasty here, as we will be a dependency of Fluffy...
+
+_test ∷ IO ()
+_test = doTest tests
+
+--------------------
+
+_tests ∷ String → IO ()
+_tests = doTestS tests
+
+_testr ∷ String → Natural → IO ()
+_testr = doTestR tests
+
+-- that's all, folks! ----------------------------------------------------------
