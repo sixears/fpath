8d15e97bc2c8bf0cd8b25b12e74474fc34f2f7aa
Mr. Fluffy <git@sixearsm>
2022-11-03 05:48:29 +0000

    base0->0.0.4.2;base0t->0.0.1.2

--- flake.nix
+++ flake.nix
@@ -3,17 +3,17 @@
 
   inputs = {
     nixpkgs.url      = "github:nixos/nixpkgs/be44bf67"; # nixos-22.05 2022-10-15
-    build-utils.url  = "github:sixears/flake-build-utils/r1.0.0.4";
+    build-utils.url  = "github:sixears/flake-build-utils/r1.0.0.5";
 
-    base1t.url               = "github:sixears/base1t/r0.0.5.5";
-    exited.url               = "github:sixears/exited/r1.0.4.6";
-    has-callstack.url        = "github:sixears/has-callstack/r1.0.1.6";
-    monaderror-io.url        = "github:sixears/monaderror-io/r1.2.5.5";
-    more-unicode.url         = "github:sixears/more-unicode/r0.0.17.3";
-    non-empty-containers.url = "github:sixears/non-empty-containers/r1.4.3.6";
-    quasiquoting.url         = "github:sixears/quasiquoting/r1.0.1.6";
-    tasty-plus.url           = "github:sixears/tasty-plus/r1.5.2.5";
-    tfmt.url                 = "github:sixears/tfmt/r0.2.7.5";
+    base1t.url               = "github:sixears/base1t/r0.0.5.6";
+    exited.url               = "github:sixears/exited/r1.0.4.7";
+    has-callstack.url        = "github:sixears/has-callstack/r1.0.1.7";
+    monaderror-io.url        = "github:sixears/monaderror-io/r1.2.5.6";
+    more-unicode.url         = "github:sixears/more-unicode/r0.0.17.4";
+    non-empty-containers.url = "github:sixears/non-empty-containers/r1.4.3.7";
+    quasiquoting.url         = "github:sixears/quasiquoting/r1.0.1.7";
+    tasty-plus.url           = "github:sixears/tasty-plus/r1.5.2.6";
+    tfmt.url                 = "github:sixears/tfmt/r0.2.7.6";
   };
 
   outputs = { self, nixpkgs, flake-utils, build-utils
