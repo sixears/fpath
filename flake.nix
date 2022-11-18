0c51e6bac6145e14675aecf22080a8dcd0e9aa61
Mr. Fluffy <git@sixearsm>
2022-11-18 08:05:07 +0000

    exited->1.0.4.16

--- flake.nix
+++ flake.nix
@@ -5,14 +5,14 @@
     nixpkgs.url      = "github:nixos/nixpkgs/be44bf67"; # nixos-22.05 2022-10-15
     build-utils.url  = "github:sixears/flake-build-utils/r1.0.0.11";
 
-    base1t.url               = "github:sixears/base1t/r0.0.5.18";
-    exited.url               = "github:sixears/exited/r1.0.4.15";
+    base1t.url               = "github:sixears/base1t/r0.0.5.19";
+    exited.url               = "github:sixears/exited/r1.0.4.16";
     has-callstack.url        = "github:sixears/has-callstack/r1.0.1.12";
     monaderror-io.url        = "github:sixears/monaderror-io/r1.2.5.13";
     more-unicode.url         = "github:sixears/more-unicode/r0.0.17.8";
-    non-empty-containers.url = "github:sixears/non-empty-containers/r1.4.3.19";
+    non-empty-containers.url = "github:sixears/non-empty-containers/r1.4.3.20";
     quasiquoting.url         = "github:sixears/quasiquoting/r1.0.1.19";
-    tasty-plus.url           = "github:sixears/tasty-plus/r1.5.2.14";
+    tasty-plus.url           = "github:sixears/tasty-plus/r1.5.2.15";
     tfmt.url                 = "github:sixears/tfmt/r0.2.7.15";
   };
 
