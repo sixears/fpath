1d22e182fbe4e7e4e43c531fe9f8ed60dfdb3f82
Mr. Fluffy <git@sixearsm>
2022-11-18 18:08:36 +0000

    has-callstack->1.0.1.13

--- flake.nix
+++ flake.nix
@@ -5,15 +5,15 @@
     nixpkgs.url      = github:nixos/nixpkgs/be44bf67; # nixos-22.05 2022-10-15
     build-utils.url  = github:sixears/flake-build-utils/r1.0.0.11;
 
-    base1t.url               = github:sixears/base1t/r0.0.5.20;
+    base1t.url               = github:sixears/base1t/r0.0.5.23;
     exited.url               = github:sixears/exited/r1.0.4.16;
-    has-callstack.url        = github:sixears/has-callstack/r1.0.1.12;
+    has-callstack.url        = github:sixears/has-callstack/r1.0.1.13;
     monaderror-io.url        = github:sixears/monaderror-io/r1.2.5.13;
     more-unicode.url         = github:sixears/more-unicode/r0.0.17.8;
-    non-empty-containers.url = github:sixears/non-empty-containers/r1.4.3.21;
-    quasiquoting.url         = github:sixears/quasiquoting/r1.0.1.19;
+    non-empty-containers.url = github:sixears/non-empty-containers/r1.4.3.23;
+    quasiquoting.url         = github:sixears/quasiquoting/r1.0.1.21;
     tasty-plus.url           = github:sixears/tasty-plus/r1.5.2.16;
-    tfmt.url                 = github:sixears/tfmt/r0.2.7.15;
+    tfmt.url                 = github:sixears/tfmt/r0.2.7.16;
   };
 
   outputs = { self, nixpkgs, build-utils
