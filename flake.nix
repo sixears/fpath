54e159a6d5496f283c6f619237ff618a909dea7b
Mr. Fluffy <git@sixearsm>
2022-11-19 09:11:17 +0000

    parsec-plus-base->1.0.5.21

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
     monaderror-io.url        = github:sixears/monaderror-io/r1.2.5.18;
     more-unicode.url         = github:sixears/more-unicode/r0.0.17.11;
     non-empty-containers.url = github:sixears/non-empty-containers/r1.4.3.31;
-    quasiquoting.url         = github:sixears/quasiquoting/r1.0.1.29;
+    quasiquoting.url         = github:sixears/quasiquoting/r1.0.1.30;
     tasty-plus.url           = github:sixears/tasty-plus/r1.5.2.21;
     tfmt.url                 = github:sixears/tfmt/r0.2.7.23;
   };
